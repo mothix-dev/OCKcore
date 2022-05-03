319ed7b029f975238b532a6c44f840a0a5c3889d
VeryCoolMoment <64923791+VeryCoolMoment@users.noreply.github.com>
2022-05-03 20:30:49 +0100

    Fixed a issue, added some copyright info

--- echo.c
+++ echo.c
@@ -1,5 +1,10 @@
-
+/*
+    OCKcore - POSIX "echo" utility
+    https://pubs.opengroup.org/onlinepubs/009604599/utilities/echo.html
+    Copyright (c) 2022 VeryCoolMoment
+*/
 #include <stdio.h>
+#include <errno.h>
 
 void error(char error[])
 {
@@ -9,11 +14,12 @@ int main( int argc, char *argv[] )  {
 
    if( argc == 2 ) {
       printf("%s\n", argv[1]);
+      return 0;
    }
-   else if( argc > 2 ) {
+   else if( argc > 1 ) {
       error("too many arguments given");
    }
    else {
-      error("no argument given or unknown error");
+      printf("\n");
    }
 }
