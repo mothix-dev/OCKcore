e9ef519d42f914ae66f82346e59b62a6c66de954
Lilly <lilly@lillycat.tech>
2022-08-02 00:35:11 +0100

    make yes FAAAAST

--- yes.c
+++ yes.c
@@ -6,15 +6,28 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define BUFSIZE 8192
 
 int main(int argc, char **argv) {
-    if(argc == 1)
-        while(1)
-            puts("y");
-    else
-        while(1)
-            puts(argv[1]);
+    char *buf   = malloc(BUFSIZE);
+    int bufused = 0;
 
-    fflush(stdout);
+    if (argc <= 1) {
+        char yes[]  = {'y', '\n'};
+        while (bufused < BUFSIZE) {
+            memcpy(buf+bufused, yes, sizeof(yes));
+            bufused += sizeof(yes);
+        }
+        while(write(1, buf, BUFSIZE));
+    }
+
+    else if (argc <= 2) {
+        while (1)
+            puts(argv[1]);
+    }
+
     return 0;
 }
