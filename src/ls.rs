e67273965cd10c163181b3bf89e05f36034cecc3
SED <sed4906birdie@gmail.com>
2023-01-16 14:00:48 -0800

    add ls in rust

---
+++ src/ls.rs
@@ -0,0 +1,44 @@
+use std::{process::exit, env, path::{PathBuf, Path}};
+
+fn help() -> ! {
+    eprintln!("Usage: ls [-1ha] <directory>");
+    exit(1);
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+    let args = args.split_at(1).1;
+    let mut options = true;
+    let mut one_per_line = false;
+    let mut show_hidden = false;
+    for arg in args {
+        match arg.as_str() {
+            "-h" if options => {help();}
+            "-1" if options => {one_per_line = true;}
+            "-a" => {show_hidden = true;}
+            "--" if options => {
+                options = false;
+            }
+            _ => {
+                options = false;
+                ls(&PathBuf::from(arg), one_per_line, show_hidden);
+            }
+        };
+    }
+    if options {
+        ls(&env::current_dir()?, one_per_line, show_hidden);
+    }
+    Ok(())
+}
+
+fn ls(pb: &Path, one_per_line: bool, show_hidden: bool) {
+    for entry in pb.read_dir().expect("could not read directory").flatten() {
+        if show_hidden || !entry.file_name().to_str().unwrap().starts_with('.') {
+            if one_per_line {
+                println!("{}", entry.file_name().to_str().unwrap());
+            } else {
+                print!("{} ", entry.file_name().to_str().unwrap());
+            }
+        }
+    }
+}
