c20815e7f0ec35d45fcae10e99c81d54994bd09d
rotom <rotom@localhost.my.domain>
2023-01-23 13:08:00 -0800

    fixed echo not printing a newline with multiple arguments

--- src/echo.rs
+++ src/echo.rs
@@ -4,16 +4,13 @@ fn main(){
     let args: Vec<String> = env::args().collect();
     let args = args.split_at(1).1;
     let mut first = true;
-    match args.len() {
-        0 => println!(),
-        1 => println!("{}",args[0]),
-        _ => for arg in args {
-            if first {
-                print!("{arg}");
-                first = false;
-            } else {
-                print!(" {arg}");
-            }
+    for arg in args {
+        if first {
+            print!("{arg}");
+            first = false;
+        } else {
+            print!(" {arg}");
         }
     }
+    println!();
 }
